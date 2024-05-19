0813e55095a1ee4a31793841871a29ff6a92c253
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-05-18 22:46:52 -0500

    creando cliente dao III

--- src/main/java/dao/ClienteDAO.java
+++ src/main/java/dao/ClienteDAO.java
@@ -5,7 +5,14 @@
 package dao;
 
 import Config.ConexionBD;
+import Models.Cliente;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -22,8 +29,63 @@ public class ClienteDAO {
     }
     
     public void crearCliente(Cliente cliente){
+        String sql= "INSERT INTO Cliente (nombre, telefono, direccion, email) VALUES (?)";
+        try(PreparedStatement statement = connection.prepareStatement(sql)){
+            statement.setString(1, cliente.getNombre());
+            statement.setString(2, cliente.getTelefono());
+            statement.setString(3, cliente.getDireccion());                      
+            statement.setString(4, cliente.getEmail());
+            statement.executeUpdate();
         
-       
+        }catch(SQLException e){
+               System.out.println("Error al guardar los datos"+ e.getMessage());    
+         }      
+    }
+    
+    public List<Cliente> listarClientes()
+    {
+        List<Cliente> listClientes = new ArrayList<>();
+        String sql = "SELECT FROM * Cliente";
+        
+        try(Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(sql))
+        {
+            while(resultSet.next())
+                    {
+                        int id = resultSet.getInt("clienteID");
+                        String nombre = resultSet.getString("nombre");
+                        String telefono = resultSet.getString("telefono");
+                        String direccion = resultSet.getString("direccion");
+                        String email = resultSet.getString("email");
+                        Cliente cliente = new Cliente(id, nombre, telefono, direccion, email);
+                        listClientes.add(cliente);
+                    }            
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Error al consultar los datos" + e.getMessage());
+        }   
+        return listClientes;    
+    }
+    
+    public void actualizarCliente(Cliente cliente)
+    {
+        String sql = ("UPDATE Cliente SET nombre = ?, telefono = ?, direccion = ?, email = ? WHERE clienteID = ?" );
+        
+        try( PreparedStatement statement = connection.prepareStatement(sql))
+        {
+            statement.setString(1, cliente.getNombre());
+            statement.setString(2, cliente.getTelefono());
+            statement.setString(3, cliente.getDireccion());
+            statement.setString(4, cliente.getEmail());
+            statement.setInt(5, cliente.getClienteID());
+            statement.executeUpdate();
+            
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Error al actualizar el cliente " + e.getMessage());
+        }
     }
     
 }
