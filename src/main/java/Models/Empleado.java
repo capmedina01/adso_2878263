c1b558e49a48dc1077fe0f28a396e4024bd3fe64
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-05-20 20:56:34 -0500

    actualizando modelo login

---
+++ src/main/java/Models/Empleado.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author kalic
+ */
+public class Empleado {
+    
+    private int empleadoID;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String email;
+    private String cargo;
+
+    public Empleado(int empleadoID, String nombre, String direccion, String telefono, String email, String cargo) {
+        this.empleadoID = empleadoID;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.email = email;
+        this.cargo = cargo;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public int getEmpleadoID() {
+        return empleadoID;
+    }
+
+    public void setEmpleadoID(int empleadoID) {
+        this.empleadoID = empleadoID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+}
---
+++ src/main/java/Models/Login.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author kalic
+ */
+public class Login {
+    int loginID;
+    String usuario;
+    String clave;
+    int empleadoID;
+
+    public Login(int loginID, String usuario, String clave, int empleadoID) {
+        this.loginID = loginID;
+        this.usuario = usuario;
+        this.clave = clave;
+        this.empleadoID = empleadoID;
+    }
+
+    public int getLoginID() {
+        return loginID;
+    }
+
+    public void setLoginID(int loginID) {
+        this.loginID = loginID;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public int getEmpleadoID() {
+        return empleadoID;
+    }
+
+    public void setEmpleadoID(int empleadoID) {
+        this.empleadoID = empleadoID;
+    }
+    
+    
+}
+
