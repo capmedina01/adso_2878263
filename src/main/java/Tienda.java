7055b13c731269f1f018bf0f11d39f501ba89a6a
perdomomedina.carlos@gmail.com <perdomomedina.carlos@gmail.com>
2024-06-04 14:44:37 -0500

    ejecucion jframe

--- src/main/java/Tienda.java
+++ src/main/java/Tienda.java
@@ -2,6 +2,7 @@
 import Controllers.CategoriaController;
 import Controllers.ProductoController;
 import Models.Categoria;
+import Views.Login;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -18,6 +19,8 @@ public class Tienda {
 
     public static void main(String[] args) { 
        
-       
+       Login login = new Login();
+       login.setVisible(true);
+       login.setLocationRelativeTo(null);
     }   
 }
